ddec137e65b0f3d96a47634638541fc0daf1cfe0
Zachary Porras <ztp567@gmail.com>
2022-08-01 16:33:16 -0400

    deployed backend to aws and updated angular url

--- ListFrontEnd/src/app/services/tutorial.service.ts
+++ ListFrontEnd/src/app/services/tutorial.service.ts
@@ -3,7 +3,7 @@ import { Tutorial } from '../models/tutorial.model';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const baseUrl='http://localhost:8080/api/tutorials';
+const baseUrl='http://tutoriallistbackendsql-env.eba-hmqrmjsb.us-east-1.elasticbeanstalk.com/api/tutorials';
 
 @Injectable({
   providedIn: 'root'
